86f44b90b67fccd87026e56ae4ca699846a25301
Max Mulatz <klappradla@gmail.com>
2015-02-12 14:24:15 +0100

    add numbers and appendDigit

--- swift_calc/ViewController.swift
+++ swift_calc/ViewController.swift
@@ -11,6 +11,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+    
+    var typingNumber = false
 
+    @IBAction func addDigit(sender: UIButton) {
+        if typingNumber {
+            display.text! += sender.currentTitle!
+        } else {
+            display.text! = sender.currentTitle!
+            typingNumber = true
+        }
+    }
 }
 
